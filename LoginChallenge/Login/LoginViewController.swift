ba5058be5845c25e2d0e3defdfeffe692c4ed4d7
hicka04 <hicka04@gmail.com>
2022-01-23 00:13:26 +0900

    Extract login logic to viewModel

--- LoginChallenge/Login/LoginViewController.swift
+++ LoginChallenge/Login/LoginViewController.swift
@@ -26,131 +26,73 @@ final class LoginViewController: UIViewController {
         passwordField.textPublisher.assign(to: &viewModel.$passwordString)
 
         viewModel.$isEnabledLoginButton.assign(to: \.isEnabled, on: loginButton).store(in: &cancellables)
+        viewModel.$isEnabledIdFeild.assign(to: \.isEnabled, on: idField).store(in: &cancellables)
+        viewModel.$isEnabledPasswordFeild.assign(to: \.isEnabled, on: passwordField).store(in: &cancellables)
+
+        viewModel.$loginState.sink { loginState in
+            switch loginState {
+            case .loggingOut:
+                break
+
+            case .loginProccessing:
+                // Activity Indicator を表示。
+                let activityIndicatorViewController: ActivityIndicatorViewController = .init()
+                activityIndicatorViewController.modalPresentationStyle = .overFullScreen
+                activityIndicatorViewController.modalTransitionStyle = .crossDissolve
+                self.present(activityIndicatorViewController, animated: true, completion: nil)
+
+            case .loggedIn:
+                self.dismiss(animated: true) {
+                    // HomeView に遷移。
+                    let destination = UIHostingController(rootView: HomeView(dismiss: { [weak self] in
+                        await self?.dismiss(animated: true)
+                    }))
+                    destination.modalPresentationStyle = .fullScreen
+                    destination.modalTransitionStyle = .flipHorizontal
+                    self.present(destination, animated: true, completion: nil)
+                }
+
+            case .loginError(let error):
+                self.dismiss(animated: true) {
+                    let title: String
+                    let message: String
+                    switch error {
+                    case is LoginError:
+                        title = "ログインエラー"
+                        message = "IDまたはパスワードが正しくありません。"
+
+                    case is NetworkError:
+                        title = "ネットワークエラー"
+                        message = "通信に失敗しました。ネットワークの状態を確認して下さい。"
+
+                    case is ServerError:
+                        title = "サーバーエラー"
+                        message = "しばらくしてからもう一度お試し下さい。"
+
+                    default:
+                        title = "システムエラー"
+                        message = "エラーが発生しました。"
+                    }
+                    let alertController: UIAlertController = .init(
+                        title: title,
+                        message: message,
+                        preferredStyle: .alert
+                    )
+                    alertController.addAction(.init(title: "閉じる", style: .default, handler: nil))
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }.store(in: &cancellables)
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        // VC を表示する前に View の状態のアップデートし、状態の不整合を防ぐ。
-        idField.isEnabled = true
-        passwordField.isEnabled = true
+        viewModel.viewWillAppear()
     }
     
     // ログインボタンが押されたときにログイン処理を実行。
     @IBAction private func loginButtonPressed(_ sender: UIButton) {
-        Task {
-            // Task.init で 1 サイクル遅れるので、
-            // その間に再度ログインボタンが押された場合に
-            // 処理が二重に実行されるのを防ぐ。
-            guard loginButton.isEnabled else { return }
-            
-            // Task.init で 1 サイクル遅れる間に、
-            // 本来処理を実行すべきでない不正な状態になっていた場合に
-            // 処理が実行されるのを防ぐ。
-            guard let id = idField.text, let password = passwordField.text else { return }
-            
-            // 処理中は入力とボタン押下を受け付けない。
-            idField.isEnabled = false
-            passwordField.isEnabled = false
-            loginButton.isEnabled = false
-            
-            // Activity Indicator を表示。
-            let activityIndicatorViewController: ActivityIndicatorViewController = .init()
-            activityIndicatorViewController.modalPresentationStyle = .overFullScreen
-            activityIndicatorViewController.modalTransitionStyle = .crossDissolve
-            await present(activityIndicatorViewController, animated: true)
-            
-            do {
-                // API を叩いて処理を実行。
-                try await AuthService.logInWith(id: id, password: password)
-                
-                // Activity Indicator を非表示に。
-                await dismiss(animated: true)
-
-                // HomeView に遷移。
-                let destination = UIHostingController(rootView: HomeView(dismiss: { [weak self] in
-                    await self?.dismiss(animated: true)
-                }))
-                destination.modalPresentationStyle = .fullScreen
-                destination.modalTransitionStyle = .flipHorizontal
-                await present(destination, animated: true)
-                
-                // HomeViewController に遷移。
-                //performSegue(withIdentifier: "Login", sender: nil)
-                
-                // この VC から遷移するのでボタンの押下受け付けは再開しない。
-                // 遷移アニメーション中に処理が実行されることを防ぐ。
-            } catch let error as LoginError {
-                // ユーザーに詳細なエラー情報は提示しないが、
-                // デバッグ用にエラー情報を表示。
-                logger.info("\(error)")
-                
-                // Activity Indicator を非表示に。
-                await dismiss(animated: true)
-                
-                // 入力とログインボタン押下を再度受け付けるように。
-                idField.isEnabled = true
-                passwordField.isEnabled = true
-                loginButton.isEnabled = true
-                
-                // アラートでエラー情報を表示。
-                // ユーザーには不必要に詳細なエラー情報は提示しない。
-                let alertController: UIAlertController = .init(
-                    title: "ログインエラー",
-                    message: "IDまたはパスワードが正しくありません。",
-                    preferredStyle: .alert
-                )
-                alertController.addAction(.init(title: "閉じる", style: .default, handler: nil))
-                await present(alertController, animated: true)
-            } catch let error as NetworkError {
-                logger.info("\(error)")
-                
-                await dismiss(animated: true)
-                
-                idField.isEnabled = true
-                passwordField.isEnabled = true
-                loginButton.isEnabled = true
-                
-                let alertController: UIAlertController = .init(
-                    title: "ネットワークエラー",
-                    message: "通信に失敗しました。ネットワークの状態を確認して下さい。",
-                    preferredStyle: .alert
-                )
-                alertController.addAction(.init(title: "閉じる", style: .default, handler: nil))
-                await present(alertController, animated: true)
-            } catch let error as ServerError {
-                logger.info("\(error)")
-                
-                await dismiss(animated: true)
-                
-                idField.isEnabled = true
-                passwordField.isEnabled = true
-                loginButton.isEnabled = true
-                
-                let alertController: UIAlertController = .init(
-                    title: "サーバーエラー",
-                    message: "しばらくしてからもう一度お試し下さい。",
-                    preferredStyle: .alert
-                )
-                alertController.addAction(.init(title: "閉じる", style: .default, handler: nil))
-                await present(alertController, animated: true)
-            } catch {
-                logger.info("\(error)")
-                
-                await dismiss(animated: true)
-                
-                idField.isEnabled = true
-                passwordField.isEnabled = true
-                loginButton.isEnabled = true
-                
-                let alertController: UIAlertController = .init(
-                    title: "システムエラー",
-                    message: "エラーが発生しました。",
-                    preferredStyle: .alert
-                )
-                alertController.addAction(.init(title: "閉じる", style: .default, handler: nil))
-                await present(alertController, animated: true)
-            }
-        }
+        viewModel.loginButtonPressed()
     }
 }
--- LoginChallenge/Login/LoginViewModel.swift
+++ LoginChallenge/Login/LoginViewModel.swift
@@ -7,19 +7,71 @@
 
 import Foundation
 import Combine
+import Entities
+import APIServices
+import Logging
+
+enum LoginState {
+    case loggingOut
+    case loginProccessing
+    case loggedIn
+    case loginError(Error)
+
+    var isLoginProcessing: Bool {
+        switch self {
+        case .loginProccessing: return true
+        default: return false
+        }
+    }
+}
 
 @MainActor
 final class LoginViewModel: ObservableObject {
     @Published var idString: String?
     @Published var passwordString: String?
     @Published private(set) var isEnabledLoginButton: Bool = false
+    @Published private(set) var isEnabledIdFeild: Bool = true
+    @Published private(set) var isEnabledPasswordFeild: Bool = true
+    @Published private(set) var loginState: LoginState = .loggingOut
+
+    private let logger: Logger = .init(label: String(reflecting: LoginViewModel.self))
 
     init() {
-        Publishers.CombineLatest(
+        Publishers.CombineLatest3(
             $idString.replaceNil(with: ""),
-            $passwordString.replaceNil(with: "")
-        ).map { idString, passwordString in
-            !idString.isEmpty && !passwordString.isEmpty
+            $passwordString.replaceNil(with: ""),
+            $loginState
+        ).map { idString, passwordString, state in
+            !idString.isEmpty && !passwordString.isEmpty && !state.isLoginProcessing
         }.assign(to: &$isEnabledLoginButton)
+
+        $loginState.map { !$0.isLoginProcessing }.assign(to: &$isEnabledIdFeild)
+        $loginState.map { !$0.isLoginProcessing }.assign(to: &$isEnabledPasswordFeild)
+        $loginState.map { !$0.isLoginProcessing }.assign(to: &$isEnabledIdFeild)
+    }
+
+    func viewWillAppear() {
+        loginState = .loggingOut
+    }
+
+    func loginButtonPressed() {
+        guard !loginState.isLoginProcessing else {
+            return
+        }
+
+        loginState = .loginProccessing
+
+        Task {
+            do {
+                // API を叩いて処理を実行。
+                try await AuthService.logInWith(id: idString!, password: passwordString!)
+
+                loginState = .loggedIn
+            } catch {
+                logger.info("\(error)")
+
+                loginState = .loginError(error)
+            }
+        }
     }
 }
